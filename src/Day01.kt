ba554c046f1d84556ae88e71f8ad002628054153
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-01 12:39:35 +0100

    Day01 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,10 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { line ->
+                (line.find { it.isDigit() }!!.digitToInt() * 10
+                + line.findLast { it.isDigit() }!!.digitToInt())  }
+            .fold(0) { s, v -> s + v }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +13,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
