144ff29ee07b759012a4a5a3355ca6ecf6129eac
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-02 14:53:01 +0100

    Day02 Part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,11 +5,21 @@ enum class Color {
 typealias Dice = Map<Color, Int>
 
 fun main() {
+    fun Dice.power() = Color.entries.fold(1) { acc, color ->
+        acc * this.getOrDefault(color, 1)
+    }
+
     data class Game(val id: Int, val sets: List<Dice>) {
         fun isPossibleWith(dice: Dice) = sets.all { set ->
             set.all { (color, count) ->
                 count <= dice.getOrDefault(color, 0)
             }
+        }
+
+        fun minimalSet(): Dice {
+            return Color.entries.map { color ->
+                color to sets.maxOf { it.getOrDefault(color, 0) }
+            }.toMap()
         }
     }
 
@@ -36,12 +46,14 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val games = input.map(::parseGame).println()
+        return games.map { it.minimalSet() }.sumOf { it.power() }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput).println() == 8)
+    check(part2(testInput).println() == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
