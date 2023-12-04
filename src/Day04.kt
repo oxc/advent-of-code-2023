57b93ce38bf602135624a3858cb2e582712bf4c8
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-04 14:46:05 +0100

    Day04 Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -12,10 +12,11 @@ fun main() {
         return Card(gameId, winningNumbers, numbers)
     }
 
+    fun Card.winners() = winningNumbers.intersect(numbers)
+
     fun part1(input: List<String>): Int {
         val cards = input.map(::parseCard).println()
 
-        fun Card.winners() = winningNumbers.intersect(numbers)
         fun Card.points(): Long {
             val winners = winners()
             return if (winners.isEmpty()) 0 else 2.pow(winners.size - 1)
@@ -25,12 +26,24 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val cards = input.mapTo(ArrayList(input.size), ::parseCard).println()
+        val counts = cards.mapTo(ArrayList(cards.size)) { 1 }
+
+        for ((i, card) in cards.withIndex()) {
+            val count = counts[i]
+            val copies = card.winners().size
+            for (copi in (i + 1)..(i + copies).coerceAtMost(counts.size - 1)) {
+                counts[copi] += count
+            }
+        }
+
+        return counts.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput).println() == 13)
+    check(part2(testInput).println() == 30)
 
     val input = readInput("Day04")
     part1(input).println()
