9992508b47335d1b5d6c084f04b40d54f966e44d
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-07 15:16:48 +0100

    Day07 Part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,15 +1,5 @@
 @JvmInline
 value class Card(val label: Char) {
-    val value
-        get() = when (label) {
-            'A' -> 14
-            'K' -> 13
-            'Q' -> 12
-            'J' -> 11
-            'T' -> 10
-            else -> label.digitToInt()
-        }
-
     override fun toString() = label.toString()
 }
 
@@ -23,41 +13,94 @@ enum class Type(val value: Int) {
     HighCard(1),
 }
 
-data class Hand(val cards: List<Card>, val bid: Int)
+data class Hand(val cards: List<Card>, val bid: Long) {
+    override fun toString() = "${cards.joinToString("")} $bid"
+}
+
+data class TypedHand(val hand: Hand, val type: Type) {
+    override fun toString() = "$hand $type"
+}
+
+interface GameRules {
+    fun Card.value(): Int
+    fun Hand.type(): Type
+}
 
 fun main() = day(7) {
     fun parseHands(input: List<String>) = input.map { line ->
-        val (sHand, sBid) = line.println().split(' ')
+        val (sHand, sBid) = line.split(' ')
         val cards = sHand.map { Card(it) }
-        Hand(cards, sBid.toInt())
+        Hand(cards, sBid.toLong())
     }
 
-    part1(check = 6440, ::parseHands) { hands ->
-        fun Hand.type(): Type {
-            val counts = cards.groupingBy { it.label }.eachCount()
-            val sortedCounts = counts.toList().sortedByDescending { it.second }
-            val (_, count) = sortedCounts.first()
-            return when (count) {
-                5 -> Type.FiveOfAKind
-                4 -> Type.FourOfAKind
-                3 -> {
-                    val (_, count2) = sortedCounts[1]
-                    if (count2 == 2) Type.FullHouse else Type.ThreeOfAKind
-                }
+    fun GameRules.winnings(hands: List<Hand>): Long {
+        val withType = hands.map { TypedHand(it, it.type()) }
+        val compareByType = compareBy<TypedHand> { it.type.value }
+        val compareByValue = (0..<5).fold(compareByType) { acc, i -> acc.thenBy { it.hand.cards[i].value() } }
+        val ranked = withType.sortedWith(compareByValue)
+        return ranked.mapIndexed { index, hand -> hand.hand.bid * (index + 1) }.sum()
+    }
 
-                2 -> {
-                    val (_, count2) = sortedCounts[1]
-                    if (count2 == 2) Type.TwoPair else Type.OnePair
-                }
+    fun typeFromTopCounts(count: Int, count2: Int): Type {
+        return when (count) {
+            5 -> Type.FiveOfAKind
+            4 -> Type.FourOfAKind
+            3 -> {
+                if (count2 == 2) Type.FullHouse else Type.ThreeOfAKind
+            }
 
-                else -> Type.HighCard
+            2 -> {
+                if (count2 == 2) Type.TwoPair else Type.OnePair
+            }
+
+            else -> Type.HighCard
+        }
+    }
+
+    part1(check = 6440L, ::parseHands) { hands ->
+        val rules = object : GameRules {
+            override fun Card.value() = when (label) {
+                'A' -> 14
+                'K' -> 13
+                'Q' -> 12
+                'J' -> 11
+                'T' -> 10
+                else -> label.digitToInt()
+            }
+
+            override fun Hand.type(): Type {
+                val sortedCounts = cards.groupingBy { it.label }.eachCount()
+                    .toList().sortedByDescending { it.second }
+                val count = sortedCounts.first().second
+                val count2 = sortedCounts.getOrNull(1)?.second ?: 0
+                return typeFromTopCounts(count, count2)
             }
         }
 
-        val compareByType = compareBy<Hand> { it.type().value }
-        val compareByValue = (0..<5).fold(compareByType) { acc, i -> acc.thenBy { it.cards[i].value } }
-        val ranked = hands.sortedWith(compareByValue)
+        rules.winnings(hands)
+    }
 
-        ranked.mapIndexed { index, hand -> hand.bid * (index + 1) }.sum()
+    part2(check = 5905L, ::parseHands) { hands ->
+        val rules = object : GameRules {
+            override fun Card.value() = when (label) {
+                'A' -> 14
+                'K' -> 13
+                'Q' -> 12
+                'J' -> 1
+                'T' -> 10
+                else -> label.digitToInt()
+            }
+
+            override fun Hand.type(): Type {
+                val counts = cards.groupingBy { it.label }.eachCountTo(mutableMapOf())
+                val jokers = counts.remove('J') ?: 0
+                val sortedCounts = counts.toList().sortedByDescending { it.second }
+                val count = sortedCounts.firstOrNull()?.second ?: 0
+                val count2 = sortedCounts.getOrNull(1)?.second ?: 0
+                return typeFromTopCounts(count + jokers, count2)
+            }
+        }
+
+        rules.winnings(hands)
     }
 }
