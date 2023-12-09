938e65e748da1c6cfb39ba5b777a43ca0e9a745f
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-09 12:00:36 +0100

    Day09 Part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -3,9 +3,9 @@ typealias History = List<Long>
 fun main() = day(9) {
     fun parseHistory(input: List<String>) = input.map { it.splitToLongs() }
 
-    part1(check = 114L, ::parseHistory) { input ->
-        fun History.findDifferences() = zipWithNext().map { (a, b) -> b - a }
+    fun History.findDifferences() = zipWithNext().map { (a, b) -> b - a }
 
+    part1(check = 114L, ::parseHistory) { input ->
         fun History.predictNextValue(): Long {
             if (all { it == 0L }) return 0
             val nextLine = findDifferences()
@@ -13,5 +13,15 @@ fun main() = day(9) {
         }
 
         input.sumOf { it.predictNextValue() }
+    }
+
+    part2(check = 2L, ::parseHistory) { input ->
+        fun History.predictEarlierValue(): Long {
+            if (all { it == 0L }) return 0
+            val nextLine = findDifferences()
+            return first() - nextLine.predictEarlierValue()
+        }
+
+        input.sumOf { it.predictEarlierValue() }
     }
 }
