e5dd5adb0b8fb2a179e0ad6bd8f1dc663a0eef0b
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-11 22:33:57 +0100

    Day11 Part 2
    Mutability, what a stupid idea, that never worked in AoC :D

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,41 +1,65 @@
 package day11
 
 import day
-import println
 import util.collection.eachUniquePair
 import util.matrix.AbstractMatrixElement
-import util.matrix.CharMatrix
-import util.parse.parseCharMatrix
+import util.matrix.Matrix
+import kotlin.math.absoluteValue
 
-typealias Universe = CharMatrix
+data class UniPoint(val value: Char, var realX: Long = -1, var realY: Long = -1) {
+    val isGalaxy = value == '#'
+    val isVoid = value != '#'
+    fun distance(other: UniPoint) = (realX - other.realX).absoluteValue + (realY - other.realY).absoluteValue
+}
+typealias Universe = Matrix<UniPoint>
 
 fun main() = day(11) {
-    fun Universe.expand() {
-        fun List<AbstractMatrixElement<Char>>.expand(add: Universe.(i: Int) -> Unit) {
-            var i = 0
-            var added = 0
-            while (i < size) {
-                if (this[i].asSequence().all { it.value != '#' }) {
-                    add(i + added)
-                    added += 1
+    fun parseUniverse(lines: List<String>) = Matrix.fromLines(lines) { UniPoint(it) }.apply {
+        asSequence().forEach {
+            it.value.realX = it.x.toLong()
+            it.value.realY = it.y.toLong()
+        }
+    }
+
+    fun Universe.expand(count: Long = 1L) {
+        fun List<AbstractMatrixElement<UniPoint>>.expand(add: Universe.(i: Int) -> Unit) {
+            forEachIndexed { i, line ->
+                if (line.asSequence().all { it.value.isVoid }) {
+                    add(i)
                 }
-                i += 1
             }
         }
 
-        rows().toList().expand { y -> insertRowAfter(y, '_') }
-        columns().toList().expand { x -> insertColumnAfter(x, ',') }
-    }
-
-    fun Universe.galaxies() = asSequence().filter { it.value == '#' }
-
-    part1(check = 374, ::parseCharMatrix) { universe ->
-        universe.println()
-        universe.expand()
-        universe.println()
-
-        universe.galaxies().toList().eachUniquePair().sumOf { (a, b) ->
-            (a - b).distance()
+        rows().toList().expand { y ->
+            this[0..<width, y + 1..<height].asSequence().forEach {
+                it.value.realY += count
+            }
+        }
+        columns().toList().expand { x ->
+            this[x + 1..<width, 0..<height].asSequence().forEach {
+                it.value.realX += count
+            }
         }
     }
+
+    fun Universe.galaxies() = asSequence().filter { it.value.isGalaxy }
+
+    fun Universe.solve(growth: Long): Long {
+        expand(growth - 1)
+
+        return galaxies().toList().eachUniquePair().sumOf { (a, b) ->
+            a.value.distance(b.value)
+        }
+    }
+
+    part1(check = 374L, ::parseUniverse) { universe ->
+        universe.solve(2)
+    }
+
+    part2(checks = mapOf(), ::parseUniverse) { universe ->
+        //universe.solve(10) // check 1030L
+        universe.solve(1_000_000) // check 8410L
+    }
+
+
 }
