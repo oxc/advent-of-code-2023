5831887da845502971380a6db6302b609c251880
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-15 21:38:11 +0100

    Day15 Part 2

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -4,6 +4,19 @@ import Input
 import day
 import println
 
+
+sealed interface Instruction {
+    val label: String
+}
+
+data class RemoveInstruction(override val label: String) : Instruction
+data class AddInstruction(override val label: String, val focalLength: Int) : Instruction
+
+data class Lens(val label: String, val focalLength: Int)
+
+typealias Box = MutableList<Lens>
+
+
 fun main() = day(15) {
     fun CharSequence.hash() = fold(0) { acc, c ->
         ((acc + c.code) * 17) % 256
@@ -15,5 +28,43 @@ fun main() = day(15) {
 
     part1(check = 1320, ::parseSequence) { seq ->
         seq.sumOf { it.hash() }
+    }
+
+    fun parseInstructions(input: Input) = parseSequence(input).map { seq ->
+        val (label, focalLength) = seq.split('-', '=')
+        if (focalLength.isEmpty()) {
+            RemoveInstruction(label)
+        } else {
+            AddInstruction(label, focalLength.toInt())
+        }
+    }
+
+    part2(check = 145L, ::parseInstructions) { init ->
+        val boxes = Array<Box>(256) { mutableListOf() }
+
+        init.forEach { inst ->
+            val bucket = inst.label.hash()
+            val box = boxes[bucket]
+            when (inst) {
+                is AddInstruction -> {
+                    val lens = Lens(inst.label, inst.focalLength)
+                    box.indexOfFirst { it.label == inst.label }.takeIf { it != -1 }?.let {
+                        box[it] = lens
+                    } ?: run {
+                        box.add(lens)
+                    }
+                }
+
+                is RemoveInstruction -> {
+                    box.removeAll { it.label == inst.label }
+                }
+            }
+        }
+
+        boxes.withIndex().sumOf { (index, box) ->
+            (index + 1) * box.withIndex().sumOf { (lensSlot, lens) ->
+                (lensSlot + 1) * lens.focalLength.toLong()
+            }
+        }
     }
 }
