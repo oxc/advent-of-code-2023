8fe98e8c947e319a7d1a85a2f5a550cf8a5181d1
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-12-23 15:48:54 +0100

    Day21 Part 2 (not!)
    This would work with infinite runtime :D Haven't figured out how do this
    effectively, how do I memoize parts of the steps and still not count
    cells double?

--- src/day21/Day21.kt
+++ src/day21/Day21.kt
@@ -2,9 +2,11 @@ package day21
 
 import Input
 import day
+import util.matrix.Direction
 import util.matrix.Field
 import util.matrix.Matrix
 import util.matrix.Point
+import util.number.absmod
 import wtf
 
 sealed class Spot(val c: Char) {
@@ -28,6 +30,29 @@ private fun Garden.countReachableInSteps(goal: Int, start: Point): Int {
     return plotsReachable.size
 }
 
+private fun Garden.getSpotType(point: Point): Spot {
+    val x = point.x absmod width
+    val y = point.y absmod height
+    return this[x, y].value
+}
+
+private fun Garden.countInfiniteReachableInSteps(goal: Int, start: Point): Int {
+    var stepsLeft = goal
+    var plotsReachable = setOf(start)
+    while (stepsLeft-- > 0) {
+        plotsReachable = plotsReachable.flatMapTo(mutableSetOf()) { plot ->
+            Direction.entries.map { dir ->
+                (plot + dir)
+            }.filter { getSpotType(it) === Plot }
+        }
+        if (stepsLeft % 1000 == 0) {
+            println("[${1 - (stepsLeft / goal) * 100}%] $stepsLeft/$goal left")
+        }
+    }
+    return plotsReachable.size
+}
+
+
 fun main() = day(21) {
     fun parseMatrix(input: Input): Pair<Garden, Point> {
         lateinit var start: Point
@@ -49,5 +74,12 @@ fun main() = day(21) {
         val goal = if (garden.width == 11) 6 else 64
 
         garden.countReachableInSteps(goal, start)
+    }
+
+    part2(check = 1594, ::parseMatrix) { (garden, start) ->
+        // small hack for the check
+        val goal = if (garden.width == 11) 50 else 26501365
+
+        garden.countInfiniteReachableInSteps(goal, start)
     }
 }
